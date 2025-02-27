0dbb914d53b7173c633d38a203e11b16fb176bc3
Brayday05 <brayantrainedgto1971@gmail.com>
2025-02-27 17:45:57 -0500

    Calculadora

---
+++ #calculadora.py
@@ -0,0 +1,22 @@
+#calculadora
+b1= int (input("escriba un numero entero "))
+f2= int(input("escriba otro numero entero "))
+resultado= b1+b2
+
+#print#
+print(f"la suma de{b1} y {b2} es {resultado}")
+
+resultado= b1-b2
+
+#print#
+print(f"la suma de{b1} y {b2} es {resultado}")
+
+resultado= b1*b2
+
+#print#
+print(f"la suma de{b1} y {b2} es {resultado}")
+
+resultado= b1/b2
+
+#print#
+print(f"la suma de{b1} y {b2} es {resultado}")
